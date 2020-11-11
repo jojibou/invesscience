0501fc6d6b5f4215f0740a4f012fe6b362038f6b
Joanna Vitiello <jvouslol@gmail.com>
2020-11-11 12:50:51 +0100

    app streamlit working with added features on model xgboost_2014_a

--- app_streamlit.py
+++ app_streamlit.py
@@ -7,7 +7,7 @@ import streamlit as st
 import os
 from invesscience.joanna_clean_data import clean_cat, clean_country, clean_state, clean_date, clean_series\
 , clean_participants, clean_amount, clean_rounds, clean_study, clean_school, clean_female,\
-clean_founded_before, clean_worked_before, clean_founder
+clean_founded_before, clean_worked_before, clean_founder, clean_degree_count
 
 df = pd.read_csv('raw_data/support/companies_test.csv')
 
@@ -21,9 +21,9 @@ def convert_bool(x):
 
 def colors(x, cut):
     if x < (3/4)*cut:
-        return "low"
+        return "lower"
     elif x > (5/4)*cut:
-        return "high"
+        return "higher"
     else:
         return "average"
 
@@ -43,7 +43,8 @@ def create_chart_cat(column,title):
     cornerRadiusTopRight=3).encode(
     y=alt.Y("acquisition_or_ipo:Q", title="Chances of IPO or Acquisition [%]"),
     x=alt.X(f"{column}:O", title=title),
-    color= "likelihood"
+    color= alt.condition(alt.datum[column] =="All", alt.value("#314e6e"), "likelihood")
+    #"likelihood"
     ).properties(width=700, height=400)
     st.write(c)
 
@@ -86,7 +87,9 @@ def create_chart_num(column,title):
     cornerRadiusTopRight=3).encode(
     y=alt.Y("acquisition_or_ipo:Q", title="Chances of IPO or Acquisition [%]"),
     x=alt.X(f"{column}:O", title=title),
-    #color= "likelihood"
+    color= alt.condition(alt.datum[column] =="All", alt.value("#314e6e"), alt.value("#4C78A8"))
+    #"likelihood"
+    #314e6e
     ).properties(width=630, height=400)
     st.write(c)
 
@@ -111,7 +114,8 @@ def create_chart_cat_num(column,title):
     cornerRadiusTopRight=3).encode(
     y=alt.Y("acquisition_or_ipo:Q", title="Chances of IPO or Acquisition [%]"),
     x=alt.X(f"{column}:O", title=title, sort=list_1),
-    color= "likelihood"
+    color= alt.condition(alt.datum[column] =="All", alt.value("#314e6e"), "likelihood")
+    #"likelihood"
     ).properties(width=700, height=400)
     st.write(c)
 
@@ -152,12 +156,14 @@ def create_chart_bin_pos(column,title):
     ,"likelihood"
     ]]
 
-    c = alt.Chart(pivot_categories.drop("All").reset_index()).mark_bar(
+    c = alt.Chart(pivot_categories.reset_index()).mark_bar(
     cornerRadiusTopLeft=3,
     cornerRadiusTopRight=3).encode(
     y=alt.Y("acquisition_or_ipo:Q", title="Chances of IPO or Acquisition [%]"),
     x=alt.X(f"{column}:O", title=title),
-    color= alt.condition(alt.datum.likelihood=="average", alt.value("#4C78A8"), alt.value("#F1853B"))
+    color= alt.condition(alt.datum[column] =="All", alt.value("#314e6e"), "likelihood")
+    #alt.condition(alt.datum.likelihood=="average", alt.value("#4C78A8"), alt.value("#F1853B"))
+
     #"likelihood"
     ).properties(width=630, height=400)
     st.write(c)
@@ -182,7 +188,7 @@ def main(reference="a"):
         if choice == "Location":
             #Country
             st.subheader("Exits according to Country")
-            create_chart_cat_neg('country_code',"Countries")
+            create_chart_cat('country_code',"Countries")
             #State
             st.subheader("Exits according to US State")
             create_chart_cat('state_code',"States")
@@ -190,24 +196,44 @@ def main(reference="a"):
         if choice == "Founding team composition":
             #Founders
             st.subheader("Exits according to number of founders in the founding team")
-            create_chart_num('founder_count', "# Founders")
+            create_chart_cat('founder_count', "# Founders")
             #Female ratio
             st.subheader("Exits according to presence of women in the founding team")
-            create_chart_bin_neg('female_ratio', "Presence of women in founding team")
+            create_chart_cat('female_ratio', "Presence of women in founding team")
 
         if choice == "Founding team education":
+            #Number of degrees
+            st.subheader("Exits according to average number of diplomas per founder")
+            create_chart_bin_pos('degree_count', "average university diplomas obtained per founder")
+
             # top 20 school
-            st.subheader("Exits according to study at top 20 universities by founders (QS ranking 2018)")
-            create_chart_bin_pos('top_20_bool', "study at top 20 university in the founding team")
+            st.subheader("Exits according to studies at top 20 universities by founders (QS ranking 2018)")
+            create_chart_bin_pos('top_20_bool', "studies at top 20 university in the founding team")
             #CS ratio
             st.subheader("Exits according to study of Computer Science by founders")
             create_chart_bin_pos('cs_bool', "CS studies in the founding team")
             #MBA bool
-            st.subheader("Exits according to MBA by founders")
-            create_chart_bin_pos('MBA_bool', "MBA in the founding team")
+            st.subheader("Exits according to MBAs completed by founders")
+            create_chart_bin_pos('MBA_bool', "MBAs in the founding team")
+
+            #Phd bool
+            st.subheader("Exits according to PhDs completed by founders")
+            create_chart_bin_pos('phd_bool', "PhDs in the founding team")
+
             #Graduate bool
-            st.subheader("Exits according to graduate degree by founders")
-            create_chart_bin_pos('graduate', "Graduate degree in the founding team")
+            st.subheader("Exits according to graduate degrees completed by founders")
+            create_chart_bin_pos('graduate', "Graduate degrees in the founding team")
+
+            #Undergraduate bool
+            st.subheader("Exits according to undergraduate degrees completed by founders")
+            create_chart_bin_pos('undergrad', "Undergraduate degrees in the founding team")
+
+            #Professional bool
+            st.subheader("Exits according to professional degrees completed by founders")
+            create_chart_bin_pos('professional', "Professional degrees in the founding team")
+
+
+
 
         if choice == "Founding team experience":
             # top 20 school
@@ -220,13 +246,13 @@ def main(reference="a"):
         if choice == "Funding":
             # rounds before a
             st.subheader("Exits according to number of funding rounds before Series A")
-            create_chart_num('rounds_before_a', "# rounds before Series A")
+            create_chart_bin_pos('rounds_before_a', "# rounds before Series A")
             # participants at series a
             st.subheader("Exits according to number of investors at Series A")
-            create_chart_num(f'participants_{reference}', "# investors at Series A")
+            create_chart_bin_pos(f'participants_{reference}', "# investors at Series A")
             # amount raised at series a
             st.subheader("Exits according to amount raised at Series A")
-            create_chart_num(f'raised_amount_usd_{reference}', "Amount raised at series A")
+            create_chart_bin_pos(f'raised_amount_usd_{reference}', "Amount raised at series A")
 
 
 
@@ -248,16 +274,21 @@ def main(reference="a"):
     if analysis == "prediction":
         st.markdown("**This model will help you decide whether you should invest in the Series A of a company of your choice.**")
 
-        pipeline = joblib.load('monday_model.joblib')
+        pipeline = joblib.load('xgboost_2014_a.joblib')
         print("loaded model")
 
         st.header("Please tell us more about your investment opportunity")
 
-        COLS = ['category_code', 'country_code', 'state_code', 'founded_at', f'timediff_founded_series_{reference}',
-                 f'time_diff_series_{reference}_now', f'participants_{reference}', f'raised_amount_usd_{reference}',
-                 'rounds_before_a', 'mean_comp_worked_before', 'founder_count', 'graduate', 'MBA_bool', 'cs_bool', 'top_20_bool', 'mean_comp_founded_before',
-                 'female_ratio',
-                 ]
+        # COLS = ['category_code', 'country_code', 'state_code', 'founded_at', f'timediff_founded_series_{reference}',
+        #          f'time_diff_series_{reference}_now', f'participants_{reference}', f'raised_amount_usd_{reference}',
+        #          'rounds_before_a', 'mean_comp_worked_before', 'founder_count', 'graduate', 'MBA_bool', 'cs_bool', 'top_20_bool', 'mean_comp_founded_before',
+        #          'female_ratio',
+        #          ]
+        COLS = ['category_code', 'country_code','state_code', 'founded_at',f'timediff_founded_series_{reference}',
+                 f'time_diff_series_{reference}_now',f'participants_{reference}', f'raised_amount_usd_{reference}',
+                 'rounds_before_a', 'mean_comp_worked_before', 'founder_count', 'degree_count','graduate',
+                 'undergrad','professional', 'MBA_bool',
+                                        'cs_bool', 'phd_bool', 'top_20_bool', 'mean_comp_founded_before', 'female_ratio']
 
 
         categories = ['advertising',
@@ -351,16 +382,17 @@ def main(reference="a"):
         st.subheader("4. Founding team information")
         founder_count = st.number_input("# founders",min_value=0,value=0)
         female_ratio = st.number_input("# female founders",min_value=0,value=0)
-        #undergrad = st.radio(label="Any founder with an undergraduate degree?", options=["Yes", "No"])
+        degree_count = st.number_input("Average # university diplomas obtained by founder",min_value=0.0,value=1.0, max_value=10.0, step=0.01)
+        undergrad = st.radio(label="Any founder with an undergraduate degree?", options=["Yes", "No"])
         graduate = st.radio(label="Any founder with a graduate degree?", options=["Yes", "No"])
-        #professional = st.radio(label="Any founder with a professional degree?", options=["Yes", "No"])
+        professional = st.radio(label="Any founder with a professional degree?", options=["Yes", "No"])
         MBA_bool = st.radio(label="Any founder with an MBA?", options=["Yes", "No"])
         cs_bool = st.radio(label="Any founder with a Computer Science degree?", options=["Yes", "No"])
-        #phd_bool = st.radio(label="Any founder with a PhD?", options=["Yes", "No"])
+        phd_bool = st.radio(label="Any founder with a PhD?", options=["Yes", "No"])
         top_20_bool = st.radio(label="Any founder studied in any of the universities below?", options=["Yes", "No"])
         st.write(universities)
         mean_comp_founded_before = st.radio(label="Any founder a founded company before this one?", options=["Yes", "No"])
-        mean_comp_worked_before = st.number_input("Average # companies founders worked at before",min_value=0.0,value=1.0, max_value=100.0, step=0.01)
+        mean_comp_worked_before = st.number_input("Average # companies founders worked at before",min_value=0.0,value=1.0, max_value=10.0, step=0.01)
 
 
         ## formatting
@@ -379,12 +411,13 @@ def main(reference="a"):
                 f'raised_amount_usd_{reference}': clean_amount(int(raised_amount_usd)),
                 f'timediff_founded_series_{reference}': timediff_founded_series,
                 'rounds_before_a': clean_rounds(int(rounds_before_a)),
+                "degree_count": clean_degree_count(int(degree_count)),
                 "graduate" : convert_bool(graduate),
-                #"undergrad" : convert_bool(undergrad),
-                #"professional" : convert_bool(professional),
+                "undergrad" : convert_bool(undergrad),
+                "professional" : convert_bool(professional),
                 "MBA_bool" : convert_bool(MBA_bool),
                 "cs_bool" : convert_bool(cs_bool),
-                #"phd_bool" : convert_bool(phd_bool),
+                "phd_bool" : convert_bool(phd_bool),
                 "founder_count" : clean_founder(int(founder_count)),
                 "top_20_bool" : convert_bool(top_20_bool),
                 "female_ratio" : clean_female(int(female_ratio)),
